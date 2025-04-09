66b0f3282dbe7ce3670ffa03236751302f30b067
Guilherme Amaral <guilhermeamaral325@gmail.com>
2025-04-09 14:33:39 -0300

    Atualização

--- scripts/script.js
+++ scripts/script.js
@@ -11,3 +11,14 @@ const observador = new IntersectionObserver((entradas) => {
   document.querySelectorAll('.fade-in').forEach((el) => {
     observador.observe(el);
   });
+
+  function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "imagens/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "imagens/close_icon.png";
+    }
+}
